f2a0934ad74bc4f1f94750e0d18aeb9533f5a0b9
MHShihan <mahmodul.no.1@gmail.com>
2025-02-19 23:04:05 +0600

    Use useNavigae hook to go back

--- src/components/userDetails/UserDetails.jsx
+++ src/components/userDetails/UserDetails.jsx
@@ -1,11 +1,24 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const UserDetails = () => {
   const { id, title } = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
   return (
     <div>
       <h1 className="text-5xl text-center mt-20">{id}</h1>
       <h2 className="text-3xl text-center">{title}</h2>
+      <div className="flex justify-center">
+        <button
+          onClick={handleGoBack}
+          className="text-center border-2 border-red-500 font-bold rounded-xl mt-4 mx-auto p-4"
+        >
+          Go Back
+        </button>
+      </div>
     </div>
   );
 };
